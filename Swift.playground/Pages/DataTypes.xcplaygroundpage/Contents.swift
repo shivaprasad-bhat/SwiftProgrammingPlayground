cb233b2724693f6449799fef045f361c296740ae
shivaprasad <shivaprasad@svbneelmane.me>
2022-01-08 23:32:47 +0530

    Data Types - Lesson 2

---
+++ Swift.playground/Pages/DataTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+// Data Types
+// Pre-built Data Types:
+// Int - integer
+// Float - floating point value
+// Double - double value
+// Bool - Boolean
+// String - Combination of characters
+// Character - Single character
+
+import Foundation
+
+
+var str: String = "Hello world!"
+let flt: Float = 22.2
+var doub: Double = 22.112
+var chr: Character = "a"
+let bo: Bool = true
+var integ: Int = 1
+
+print(str, flt, doub, chr, bo, integ)
