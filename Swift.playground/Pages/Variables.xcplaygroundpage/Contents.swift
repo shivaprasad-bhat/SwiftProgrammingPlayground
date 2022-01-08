3927ad0843af0cb9c5ad9fb9337d6e934f0f4a52
shivaprasad <shivaprasad@svbneelmane.me>
2022-01-08 23:27:03 +0530

    Variables Playground - Lesson 1

---
+++ Swift.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Types of variable
+// var -> Modifyable
+// let -> Constant
+var greeting: String = "Hello, playground"
+print(greeting)
+
+var num1: Int = 10
+var num2: Int = 20
+print(num1 + num2)
+
+// greeting = 12   // Gives compilation error - Data Type Mistmatch
+let constant1 = 20
+// constant1 = 3        // Not possible, let for constant
+var car = "Suzuki"
+var maruti = car
+print(maruti)
