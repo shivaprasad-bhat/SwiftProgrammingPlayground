2b47c7cb1dc7fc558133a9aa1118f69e20857b67
shivaprasad <shivaprasad@svbneelmane.me>
2022-01-09 00:15:31 +0530

    Repeat While Loop & While Loop - Lesson 5

--- Swift.playground/Pages/Loops.xcplaygroundpage/Contents.swift
+++ Swift.playground/Pages/Loops.xcplaygroundpage/Contents.swift
@@ -17,3 +17,20 @@ for index in 1...5 {
     sum += index
 }
 print("Sum is", sum,"at the end of loop")
+
+
+// While Loop
+
+var counter = 5
+while (counter > 0) {
+    print("WL - Greater than 0")
+    counter -= 1
+}
+
+// Repeat-While Loop
+counter = 5
+repeat {
+    print("RWL - Greater than 0")
+    counter -= 1
+}while(counter > 0)
+
